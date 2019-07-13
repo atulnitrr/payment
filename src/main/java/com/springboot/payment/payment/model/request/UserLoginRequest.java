674eb3f7c8e261867fc434cde6b70303fb57e5e1
atulnitrr <atulkumar458@gmail.com>
2019-07-13 15:12:35 +0530

    Added validation

--- src/main/java/com/springboot/payment/payment/model/request/UserLoginRequest.java
+++ src/main/java/com/springboot/payment/payment/model/request/UserLoginRequest.java
@@ -1,8 +1,18 @@
 package com.springboot.payment.payment.model.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
 public class UserLoginRequest {
 
+    @Email(message = "Invalid email")
+    @NotNull(message = "email can not be empty")
     private String email;
+
+    @NotNull
+    @Size(min = 3, max = 13, message = "password can be between 3 and 13 char")
     private String password;
 
     public String getEmail() {
--- src/main/java/com/springboot/payment/payment/model/request/UserRequest.java
+++ src/main/java/com/springboot/payment/payment/model/request/UserRequest.java
@@ -1,9 +1,22 @@
 package com.springboot.payment.payment.model.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
 public class UserRequest {
 
+    @NotNull(message = "name can not be null")
+    @Size(min = 2, max = 88, message = "name should  be between 3 and 88 char")
     private String name;
+
+    @Email
+    @NotNull
     private String email;
+
+    @NotNull
+    @Size(min = 3, max = 15, message = "password should be between 3 and 15 char")
     private String password;
 
     public String getName() {
